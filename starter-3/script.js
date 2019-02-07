b5c975318849638deefcfa10192f3333b6b9aaf6
JUN <soipon0@gmail.com>
2019-02-07 23:49:38 +0900

    this の使い方について

--- starter-3/script.js
+++ starter-3/script.js
@@ -2,35 +2,35 @@
 // Lecture: Hoisting
 
 //functions
-calculateAge(1965);
-
-function calculateAge(year) {
-    console.log(2016 - year);
-}
-
-//retirement(1956);
-
-
-var retirement = function (year) {
-    console.log(65 - (2016 - year));
-}
-
-
-
-//variables
-
-console.log(age);
-var age = 23;
-
-function foo() {
-    console.log(age);
-    var age = 65;
-    console.log(age);
-    return age
-}
-
-foo();
-console.log(age);
+//calculateAge(1965);
+//
+//function calculateAge(year) {
+//    console.log(2016 - year);
+//}
+//
+////retirement(1956);
+//
+//
+//var retirement = function (year) {
+//    console.log(65 - (2016 - year));
+//}
+//
+//
+//
+////variables
+//
+//console.log(age);
+//var age = 23;
+//
+//function foo() {
+//    console.log(age);
+//    var age = 65;
+//    console.log(age);
+//    return age
+//}
+//
+//foo();
+//console.log(age);
 
 
 ///////////////////////////////////////
@@ -39,46 +39,92 @@ console.log(age);
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+//var a = 'Hello!';
+//first();
+//
+//function first() {
+//    var b = 'Hi!';
+//    second();
+//
+//    function second() {
+//        var c = 'Hey!';
+//        console.log(a + b + c);
+//    }
+//}
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
-first();
+//
+//var a = 'Hello!';
+//first();
+//
+//function first() {
+//    var b = 'Hi!';
+//    second();
+//
+//    function second() {
+//        var c = 'Hey!';
+//        third()
+//    }
+//}
+//
+//function third() {
+//    var d = 'John';
+////     console.log(a + b + c + d);
+//  console.log(a+d);
+//}
 
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
-
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+//console.log(this);
+
+//calculateAge(1985);
+//
+//function calculateAge(year) {
+//  console.log(2016-year);
+//  console.log(this);
+//}
+
+
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2016 - this.yearOfBirth);
+    /*
+    function innerFunction() {
+      console.log(this);
+    }
+    innerFunction();
+    */
+  }
+}
+
+john.calculateAge();
+
+var mike = {
+  name: 'Mike',
+  yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+
+
+
+
+
+
+
+
+
