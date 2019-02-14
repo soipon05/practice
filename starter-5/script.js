9d0820ef5d25a7cf9394a761c748fb2261751d48
JUN <soipon0@gmail.com>
2019-02-15 08:19:19 +0900

    更新

--- starter-5/script.js
+++ starter-5/script.js
@@ -151,3 +151,24 @@ console.log(ages);
 console.log(fullAges);
 console.log(rates);
 */
+
+
+function interviewQuestion(job) {
+  if(job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you plz explain what UX design is?');
+    }
+  }else if(job === 'teacher'){
+    return function(name){
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', What do you do?');
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+
+teacherQuestion('John');
