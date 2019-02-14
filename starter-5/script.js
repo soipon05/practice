bda91c876e72f125f49b7c9f3a0100b5c41b2123
soipon05 <33271639+soipon05@users.noreply.github.com>
2019-02-14 18:02:06 +0900

    first commit
    first commit

---
+++ starter-5/script.js
@@ -0,0 +1,131 @@
+//Function constructor
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+
+}
+
+
+Person.prototype.calculateAge =
+    function () {
+        console.log(2018 - this.yearOfBirth);
+    };
+
+Person.prototype.lastName = 'Smith';
+
+
+
+var john = new Person('john', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+*/
+
+//Object.create
+/*
+var personProto = {
+    calculateAge: function () {
+        console.log(2018 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: {
+        value: 'Jane'
+    },
+    yearOfBirth: {
+        value: 1969
+    },
+    job: {
+        value: 'designer'
+    }
+
+});
+
+*/
+
+
+/*
+//Primitives vs objects
+
+
+//primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+
+
+
+
+
+
+//objects
+
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    console.log(age + "check")
+    var age = 30;
+    //    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
+Objectは参照渡し、プリミティブ型は値渡しなのでchangeでプリミティブなageを書き換えて保存することはできない。
+*/
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0, i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2018 - el;
+}
