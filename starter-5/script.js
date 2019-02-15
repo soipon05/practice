f8e997d24d2b7d442fb7632a5767dd543db45c4b
soipon05 <33271639+soipon05@users.noreply.github.com>
2019-02-15 18:27:51 +0900

    更新
    クロージャとcallとbindとapplyについて

--- starter-5/script.js
+++ starter-5/script.js
@@ -153,22 +153,149 @@ console.log(rates);
 */
 
 
-function interviewQuestion(job) {
-  if(job === 'designer') {
-    return function(name) {
-      console.log(name + ', can you plz explain what UX design is?');
-    }
-  }else if(job === 'teacher'){
-    return function(name){
-      console.log('What subject do you teach, ' + name + '?');
-    }
-  } else {
-    return function(name) {
-      console.log('Hello ' + name + ', What do you do?');
-    }
-  }
-}
+// function interviewQuestion(job) {
+//     if (job === 'designer') {
+//         return function (name) {
+//             console.log(name + ', can you plz explain what UX design is?');
+//         }
+//     } else if (job === 'teacher') {
+//         return function (name) {
+//             console.log('What subject do you teach, ' + name + '?');
+//         }
+//     } else {
+//         return function (name) {
+//             console.log('Hello ' + name + ', What do you do?');
+//         }
+//     }
+// }
 
-var teacherQuestion = interviewQuestion('teacher');
+// var teacherQuestion = interviewQuestion('teacher');
 
-teacherQuestion('John');
+// teacherQuestion('John');
+
+
+
+/**************************************
+ * Lecture: Closures
+ */
+
+//  function retirement(retirementAge) {
+//      var a = 'years left until retirement.';
+//      return function(yearOfBirth) {
+//          var age = 2018 - yearOfBirth;
+//          console.log((retirementAge - age) + a);
+//      }
+//  }
+
+//  var retirementUs = retirement(66);
+//  retirementUs(1990);
+
+//  var retirementGermany = 
+//  retirement(65);
+//  var retirementIceland =
+//  retirement(67);
+//  retirementGermany(1990);
+//  retirementUs(1990);
+//  retirementIceland(1990);
+
+
+
+//  function interviewQuestion(job) {
+//      return function(name) {
+//          if(job === 'designer') {
+//             console.log(name + ', can you plz explain what UX design is?');
+//          }else if (job === 'teacher'){
+//             console.log('What subject do you teach, ' + name + '?');
+//          }else {
+//             console.log('Hello ' + name + ', What do you do?');
+//          }
+//      }
+//  }
+
+// interviewQuestion('teacher')('John');
+
+
+
+ //retirement(66)(1991);
+
+ /***********************
+  * Lecture: Bind, call and apply
+  */
+
+  var john = {
+      name: 'John',
+      age: 27,
+      job: 'teacher',
+      presentation: function(style, timeOfDay){
+          if(style === 'formal') {
+              console.log('Good' + timeOfDay + ', Ladies and Gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m' + this.age +'years old.');
+          } else if (style === 'friendly') {
+              console.log("Hey! What's UP? I'm " + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age +'years old.' + ' Have a nice ' +timeOfDay);
+          }
+      }
+    };
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+
+john.presentation('formal', 'mornig');
+
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoo']);
+
+var johnFriendly = 
+john.presentation.bind(john,'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFomal = 
+john.presentation.bind(emily,'formal');
+
+emilyFomal('afternoon');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
