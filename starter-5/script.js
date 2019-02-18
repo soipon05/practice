2d797326b8e66f7080e6239ca5bc8db113f4d70c
soipon05 <33271639+soipon05@users.noreply.github.com>
2019-02-18 18:04:50 +0900

    関数のまとめ
    関数を使ったチャレンジ問題の途中です。

--- starter-5/script.js
+++ starter-5/script.js
@@ -222,79 +222,272 @@ console.log(rates);
   * Lecture: Bind, call and apply
   */
 
-  var john = {
-      name: 'John',
-      age: 27,
-      job: 'teacher',
-      presentation: function(style, timeOfDay){
-          if(style === 'formal') {
-              console.log('Good' + timeOfDay + ', Ladies and Gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m' + this.age +'years old.');
-          } else if (style === 'friendly') {
-              console.log("Hey! What's UP? I'm " + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age +'years old.' + ' Have a nice ' +timeOfDay);
-          }
-      }
-    };
+//   var john = {
+//       name: 'John',
+//       age: 27,
+//       job: 'teacher',
+//       presentation: function(style, timeOfDay){
+//           if(style === 'formal') {
+//               console.log('Good' + timeOfDay + ', Ladies and Gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m' + this.age +'years old.');
+//           } else if (style === 'friendly') {
+//               console.log("Hey! What's UP? I'm " + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age +'years old.' + ' Have a nice ' +timeOfDay);
+//           }
+//       }
+//     };
 
-var emily = {
-    name: 'Emily',
-    age: 35,
-    job: 'designer'
-};
-
-
-john.presentation('formal', 'mornig');
+// var emily = {
+//     name: 'Emily',
+//     age: 35,
+//     job: 'designer'
+// };
 
 
-john.presentation.call(emily, 'friendly', 'afternoon');
-
-//john.presentation.apply(emily, ['friendly', 'afternoo']);
-
-var johnFriendly = 
-john.presentation.bind(john,'friendly');
-
-johnFriendly('morning');
-johnFriendly('night');
-
-var emilyFomal = 
-john.presentation.bind(emily,'formal');
-
-emilyFomal('afternoon');
+// john.presentation('formal', 'mornig');
 
 
+// john.presentation.call(emily, 'friendly', 'afternoon');
+
+// //john.presentation.apply(emily, ['friendly', 'afternoo']);
+
+// var johnFriendly = 
+// john.presentation.bind(john,'friendly');
+
+// johnFriendly('morning');
+// johnFriendly('night');
+
+// var emilyFomal = 
+// john.presentation.bind(emily,'formal');
+
+// emilyFomal('afternoon');
 
 
+// var years = [1990, 1965, 1937, 2005, 1998];
 
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+// //arrRes.push(fn(arr[i]));
 
+// function calculateAge(el) {
+//     return 2018 - el;
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function isFullAge(limit,el) {
+//     return el >= limit;
+// }
 
 
 
+// var ages = arrayCalc(years, calculateAge);
 
-
-
-
-
-
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+// //isFullAge.bind(this, 20)はisFullAgeをlimit20まで縛っておいて
+// console.log(ages);
+// console.log(fullJapan);
 
 
 
 
+/***************************************
+ * CODING CHALLENGE
+ */
 
+ /*
+ --- Let's build a fun quiz game in the console! ---
+
+ 1.Build a function constructor called Question to describe a question. A question should include:
+ a) question itself
+ b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+ c) correct answer (I would usera number for this)
+
+ 2. Create a couple of questions using the constructor
+
+ 3. Store them all inside an array
+
+ 4. Select one random question and log it on the conssole, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+ 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+ 6. Check if the answer is correct and print to the console whether the answer is correct it nor (Hint: write another method for this).
+
+ 7. Suppose this code would be a plugin be a plugin for other programmers to user in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+ おそらくクロージャのこと？
+ */
+
+
+
+
+
+
+
+
+// var Question = function (question, ans1, ans2) {
+//     this.question = question;
+//     this.ans1 = ans1;
+//     this.ans2 = ans2;
+// }
+
+
+// (function() {
+//     function Question(question, answers, correct) {
+//         this.question = question;
+//         this.answers = answers;
+//         this.correct = correct;
+//     }
+    
+//     Question.prototype.displayQuestion = function() {
+//         console.log(this.question);
+    
+//         for (var i = 0; i < this.answers.length; i++) {
+//             console.log(i + ':' + this.answers[i]);
+//         }
+//     }
+    
+//     Question.prototype.checkAnswer = 
+//     function(ans) {
+//         if (ans === this.correct) {
+//             console.log('Correct answer!');
+//         }else {
+//             console.log('Wrong answer. Try again!')
+//         }
+//     }
+    
+//     var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+//                                    ['YEs', 'No'],
+//                                    0);
+    
+//     var q2 = new Question('What is the name of this course\'s teacher?',
+//                                     ['John','Michael','Jonas'],
+//                                     2);
+    
+//     var q3 = new Question('What does best describe coding?',
+//                                     ['Boring','Hard',
+//                                 'Fun','Tedius'],
+//                                 2);
+    
+//     var questions = [q1, q2, q3];
+    
+//     var n = Math.floor(Math.random() * questions.length);
+    
+//     questions[n].displayQuestion();
+    
+//     var answer = parseInt(prompt('Please select the correct answer.'));
+    
+//     questions[n].checkAnswer(answer);
+    
+// })();
+
+
+
+/*
+--- Expert level ---
+
+8. After you display the result,
+display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Becareful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. Inthis case, DON'T call the function from task 8.
+
+10. Track the user's score to make the fame more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to user the power of closures for this, but you don't have to , just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+
+
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+    
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+    
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ':' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.checkAnswer = 
+    function(ans, callback) {
+        var sc;
+
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            sc = callback(true);
+        }else {
+            console.log('Wrong answer. Try again!');
+            sc = callback(false);
+        }
+
+
+        this.displayScore(sc);
+        
+    }
+    
+    Question.prototype.displayScore = 
+    function(score) {
+        console.log('Your current score is: ' + socre);
+        console.log('--------------------------------------');
+    }
+
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                                   ['YEs', 'No'],
+                                   0);
+    
+    var q2 = new Question('What is the name of this course\'s teacher?',
+                                    ['John','Michael','Jonas'],
+                                    2);
+    
+    var q3 = new Question('What does best describe coding?',
+                                    ['Boring','Hard',
+                                'Fun','Tedius'],
+                                2);
+
+
+    var questions = [q1, q2, q3];
+
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
+                                
+    function nextQuestion() {
+    
+        var n = Math.floor(Math.random() * questions.length);
+    
+        questions[n].displayQuestion();
+    
+        var answer = prompt('Please select the correct answer.');
+    
+        questions[n].checkAnswer(answer);
+
+        if(answer !== 'exit') {
+            
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+            
+            nextQuestion();
+        }
+
+    
+    
+    }
+    nextQuestion();
+
+})();
 
 
 
