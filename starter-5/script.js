c31baf287f2c8b2b6dc8f51a71d005b44ff93956
soipon05 <33271639+soipon05@users.noreply.github.com>
2019-02-19 18:56:18 +0900

    更新
    更新

--- starter-5/script.js
+++ starter-5/script.js
@@ -398,96 +398,96 @@ display the next random question, so that the game never ends (Hint: write a fun
 
 
 
-(function() {
-    function Question(question, answers, correct) {
-        this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-    }
+// (function() {
+//     function Question(question, answers, correct) {
+//         this.question = question;
+//         this.answers = answers;
+//         this.correct = correct;
+//     }
     
-    Question.prototype.displayQuestion = function() {
-        console.log(this.question);
+//     Question.prototype.displayQuestion = function() {
+//         console.log(this.question);
     
-        for (var i = 0; i < this.answers.length; i++) {
-            console.log(i + ':' + this.answers[i]);
-        }
-    }
+//         for (var i = 0; i < this.answers.length; i++) {
+//             console.log(i + ':' + this.answers[i]);
+//         }
+//     }
     
-    Question.prototype.checkAnswer = 
-    function(ans, callback) {
-        var sc;
+//     Question.prototype.checkAnswer = 
+//     function(ans, callback) {
+//         var sc;
 
-        if (ans === this.correct) {
-            console.log('Correct answer!');
-            sc = callback(true);
-        }else {
-            console.log('Wrong answer. Try again!');
-            sc = callback(false);
-        }
+//         if (ans === this.correct) {
+//             console.log('Correct answer!');
+//             sc = callback(true);
+//         }else {
+//             console.log('Wrong answer. Try again!');
+//             sc = callback(false);
+//         }
 
 
-        this.displayScore(sc);
+//         this.displayScore(sc);
         
-    }
+//     }
     
-    Question.prototype.displayScore = 
-    function(score) {
-        console.log('Your current score is: ' + socre);
-        console.log('--------------------------------------');
-    }
+//     Question.prototype.displayScore = 
+//     function(score) {
+//         console.log('Your current score is: ' + socre);
+//         console.log('--------------------------------------');
+//     }
 
 
-    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
-                                   ['YEs', 'No'],
-                                   0);
+//     var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+//                                    ['YEs', 'No'],
+//                                    0);
     
-    var q2 = new Question('What is the name of this course\'s teacher?',
-                                    ['John','Michael','Jonas'],
-                                    2);
+//     var q2 = new Question('What is the name of this course\'s teacher?',
+//                                     ['John','Michael','Jonas'],
+//                                     2);
     
-    var q3 = new Question('What does best describe coding?',
-                                    ['Boring','Hard',
-                                'Fun','Tedius'],
-                                2);
+//     var q3 = new Question('What does best describe coding?',
+//                                     ['Boring','Hard',
+//                                 'Fun','Tedius'],
+//                                 2);
 
 
-    var questions = [q1, q2, q3];
+//     var questions = [q1, q2, q3];
 
-    function score() {
-        var sc = 0;
-        return function(correct) {
-            if (correct) {
-                sc++;
-            }
-            return sc;
-        }
-    }
+//     function score() {
+//         var sc = 0;
+//         return function(correct) {
+//             if (correct) {
+//                 sc++;
+//             }
+//             return sc;
+//         }
+//     }
 
-    var keepScore = score();
+//     var keepScore = score();
                                 
-    function nextQuestion() {
+//     function nextQuestion() {
     
-        var n = Math.floor(Math.random() * questions.length);
+//         var n = Math.floor(Math.random() * questions.length);
     
-        questions[n].displayQuestion();
+//         questions[n].displayQuestion();
     
-        var answer = prompt('Please select the correct answer.');
+//         var answer = prompt('Please select the correct answer.');
     
-        questions[n].checkAnswer(answer);
+//         questions[n].checkAnswer(answer);
 
-        if(answer !== 'exit') {
+//         if(answer !== 'exit') {
             
-            questions[n].checkAnswer(parseInt(answer), keepScore);
+//             questions[n].checkAnswer(parseInt(answer), keepScore);
             
-            nextQuestion();
-        }
+//             nextQuestion();
+//         }
 
     
     
-    }
-    nextQuestion();
+//     }
+//     nextQuestion();
 
-})();
+// })();
 
 
 
