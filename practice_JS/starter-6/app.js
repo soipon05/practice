d94d98e407dfbecd92650cde8109114046151012
soipon05 <33271639+soipon05@users.noreply.github.com>
2019-02-25 15:49:37 +0900

    タイポによるエラーの修正
    タイポによるエラーの修正
    underbarが__2つ必要だが1本しかなかった
    functionに()を付け忘れたため取得するデータの中身が空になってしまった。

--- practice_JS/starter-6/app.js
+++ practice_JS/starter-6/app.js
@@ -1,8 +1,6 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
-
     //some code
-
 })();
 
 //UI CONTROLLER
@@ -11,26 +9,28 @@ var UIController = (function () {
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
-        inputValue: '.add_value'
-    }
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
     return {
         getInput: function () {
             return {
-
                 type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
-
             };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
         }
     };
-
 })();
-
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function () {
         //1. get the field input data
@@ -44,19 +44,14 @@ var controller = (function (budgetCtrl, UICtrl) {
         //4. Calculate the budget
 
         //5.Display the budget on the UI
-        console.log('It works.')
+
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
-
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
-
         }
-
     });
-
-
 })(budgetController, UIController);
