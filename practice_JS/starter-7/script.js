1a29552d756b77f0a9bf257abcb95a811da40d14
JUN <soipon0@gmail.com>
2019-03-06 20:49:00 +0900

    クラスの定義とクラスの継承について

--- practice_JS/starter-7/script.js
+++ practice_JS/starter-7/script.js
@@ -41,8 +41,6 @@
 
 // driversLicence6(true);
 
-
-
 // let i = 23;
 
 // for (let i = 0; i < 5; i++) {
@@ -56,7 +54,6 @@
 
 ///////////////////////////////////////
 // Lecture: Blocks and IIFEs
-
 
 // //ES6
 // {
@@ -103,7 +100,6 @@
 // //repeatは同一動作を引数の回数だけ行う
 // console.log(`${firstName} `.repeat(5));
 
-
 ////////////////////////////////
 // Lecture: Arrow functions
 
@@ -136,36 +132,8 @@
 ////////////////////////////
 // Lecture: Arrow functions 2
 
-
 //ES5
-var box5 = {
-    color: "green",
-    position: 1,
-    clickMe: function () {
-        //ここでselfを定義しておかないとthisを使ったときにwindowオブジェクトを参照しに行ってしまうので定義しておく
-        var self = this;
-        document.querySelector(".green").addEventListener("click", function () {
-            var str = "This is box number " + self.position + " add it is " + self.color;
-            alert(str);
-        });
-    }
-}
-
-// box5.clickMe();
-
-//ES6
-
-var box6 = {
-    color: "green",
-    position: 1,
-    clickMe: function () {
-        //ここでselfを定義しておかないとthisを使ったときにwindowオブジェクトを参照しに行ってしまうので定義しておく
-        document.querySelector(".green").addEventListener("click", () => {
-            var str = "This is box number " + this.position + " add it is " + this.color;
-            alert(str);
-        });
-    }
-}
+// d
 //arrow functionを使うとthisがもっとわかりやすい使い方になる
 // box6.clickMe();
 
@@ -210,15 +178,12 @@ var box6 = {
 //         var arr = friends.map(el =>
 //             `${this.name} is friends with ${el}`);
 
-
 //         console.log(arr);
 //     }
-
 
 // new Person("Mike").myFriends5(friends);
 /////////////////////////
 // Destructuring　分割代入
-
 
 // //ES5
 // var john = ["John", 26];
@@ -280,7 +245,6 @@ var box6 = {
 // //ES5
 // // for (var i = 0, len = boxesArr5.length; i < len; i++) {
 
-
 // //     if (boxesArr5[i].className === "box blue") {
 // //         continue;
 // //     }
@@ -288,7 +252,6 @@ var box6 = {
 // //     boxesArr5[i].textContent = "I changed to blue!";
 
 // // }
-
 
 // //ES6
 // for (const cur of boxesArr6) {
@@ -348,7 +311,6 @@ var box6 = {
 
 // // Array.from(all).forEach(cur => cur.style.color = "purple");
 
-
 // ///////////////////////////////////
 // // Lecture: Rest parameters
 
@@ -382,7 +344,6 @@ var box6 = {
 // //spreadとrest parametersの違いは？restの方は関数の宣言の時に使う
 // //spreadは関数実行の時に使う
 
-
 // function isFullAge5(limit) {
 //     // console.log(arguments);
 //     //第二引数で指定した位置からコピーが始まる
@@ -411,7 +372,7 @@ var box6 = {
 
 //     lastName === undefined ? lastName = "Smith": lastName;
 //     nationality === undefined ? nationality = "american" : nationality;
-    
+
 //     this.firstName = firstName;
 //     this.lastName = lastName;
 //     this.yearOfBirth = yearOfBirth;
@@ -432,17 +393,253 @@ var box6 = {
 
 // var emily = new SmithPerson("Emily", 1983, "Diaz", "spanish");
 
-
-
 ////////////////////////////////////
 // Lecture: Maps
 
-const question = new Map();
-question.set("question", "What is the officail name of the lateste major JavaScript");
-question.set(1, "ES5");
-question.set(2, "ES6");
-question.set(3, "ES2015");
-question.set(4, "ES7");
-question.set("correct", 3);
-question.set(true,"Correct anwear :D");
-question.set(false, "Wrong , please try again!");
+// const question = new Map();
+// question.set(
+//   "question",
+//   "What is the officail name of the lateste major JavaScript version?"
+// );
+// question.set(1, "ES5");
+// question.set(2, "ES6");
+// question.set(3, "ES2015");
+// question.set(4, "ES7");
+// question.set("correct", 3);
+// question.set(true, "Correct anwear :D");
+// question.set(false, "Wrong , please try again!");
+
+// console.log(question.get("question"));
+// //What is the official name of the latest major JavaScript
+// // console.log(question.size);
+// //8
+// if (question.has(4)) {
+//   //   question.delete(4);
+//   //   console.log("Answer 4 is here");
+// }
+
+// // // question.clear();
+
+// // question.forEach((value, key) =>
+// //   console.log(`This is ${key}, and it's set to ${value}`)
+// // );
+
+// for (let [key, value] of question.entries()) {
+//   if (typeof key === "number") {
+//     console.log(`Answer ${key}:${value}`);
+//   }
+// }
+
+// const ans = parseInt(prompt("Write the correct answer"));
+// //question.get(   {ans === question.get('correct')} ) {}内の値が合致してれば{}はtrueになるから
+// //question.get(true)の値を持ってきてconsoleに表示させる
+// console.log(question.get(ans === question.get(`correct`)));
+
+/////////////////////////////////////
+// Lecture: Classes
+
+// //ES5
+// var Person5 = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// };
+
+// Person5.prototype.calculateAge = function() {
+//   var age = new Date().getFullYear - this.yearOfBirth;
+//   console.log(age);
+// };
+
+// var john5 = new Person5("John", 1990, "teacher");
+
+// //new Date().getFullYear();これで現在の年が表示される()忘れがち
+// //ES6
+
+// class Person6 {
+//   constructor(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+//   }
+//   calculateAge() {
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+//   }
+
+//   static greeting() {
+//     console.log("Hey there");
+//   }
+// }
+
+// const john6 = new Person6("John", 1990, "teacher");
+
+// Person6.greeting();
+
+/////////////////////////////////
+// Lecture: Classes and subclasses
+
+// //ES5
+// var Person5 = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// };
+
+// Person5.prototype.calculateAge = function() {
+//   var age = new Date().getFullYear() - this.yearOfBirth;
+//   console.log(age);
+// };
+
+// var Athlete5 = function(name, yearOfBirth, job, olymicGames, medals) {
+//   Person5.call(this, name, yearOfBirth, job);
+//   this.olymicGames = olymicGames;
+//   this.medals = medals;
+// };
+
+// Athlete5.prototype = Object.create(Person5.prototype);
+// //Object.createする前にprototypeでメソッドを書くと上書きされてしまうのかな
+// //普通にまずprototype作成が終わってからメソッドを追加すればよさげ
+// Athlete5.prototype.wonMedal = function() {
+//   this.medals++;
+//   console.log(this.medals);
+// };
+
+// var johnAthlete5 = new Athlete5("John", 1990, "Swimer", 3, 10);
+
+// johnAthlete5.calculateAge();
+// johnAthlete5.wonMedal();
+
+// //ES6
+
+// class Person6 {
+//   constructor(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+//   }
+//   calculateAge() {
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+//   }
+
+//   static greeting() {
+//     console.log("Hey there");
+//   }
+// }
+
+// class Athlete6 extends Person6 {
+//   constructor(name, yearOfBirth, job, olymicGames, medals) {
+//     super(name, yearOfBirth, job);
+//     this.olymicGames = olymicGames;
+//     this.medals = medals;
+//   }
+
+//   wonMedal() {
+//     this.medals++;
+//     console.log(this.medals);
+//   }
+// }
+
+// const johnAthlete6 = new Athlete6("John", 1990, "Swimmer", 3, 10);
+
+// johnAthlete6.wonMedal();
+// johnAthlete6.calculateAge();
+// //クラスの継承についてでした
+
+/*
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+小さな2つの町の管理者でかつその公園と道の管理をしてる
+1.Parks
+2.Streets
+
+小さい町だから3つの公園と4つの道しかない。そして全ての公園と道には名前と設立年がそんざいする。Streetsって建物のこと？？
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+年の終わりにボスが最終報告を上げろと言ってその内容が、
+1．樹木の密度はそれぞれの公園でどれぐらいか
+2．公園の平均年齢？はいくつか
+3．1000本以上の樹を所持している公園は？
+4．町にある道の総距離&平均距離
+5．道にはサイズが決まっていてわからなければ最初はnormalにすること
+At an end-of-year meeting, your boss wants a final report with the following:
+1.Tree density of each park in the town (formula: number of trees/park area)
+2.Average age of each town's park (formula: sum of all ages/number of parks)
+3.The name of the park that has more than 1000 trees
+4.Total and average length of the town's streets
+5.Size classification of all streets:
+tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+*/
+
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+}
+
+class Park extends Element {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTrees = numTrees;
+  }
+
+  treeDensity() {
+    const density = this.numTrees / this.area;
+    console.log(
+      `${this.name} has a tree density of ${density} trees per square km.`
+    );
+  }
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear);
+    this.length = length;
+    this.size = size;
+  }
+
+  classifyStreet() {
+    const classification = new Map();
+    classification.set(1, "tiny");
+    classification.set(2, "small");
+    classification.set(3, "normal");
+    classification.set(4, "big");
+    classification.set(5, "huge");
+    console.log(
+      `${this.name}, build in ${this.buildYear}, is a ${classification.get(
+        this.size
+      )} street.`
+    );
+  }
+}
+
+const allParks = [
+  new Park("Green Park", 1987, 0.2, 215),
+  new Park("National Park", 1894, 2.9, 354),
+  new Park("Oak Park", 1953, 0.4, 949)
+];
+
+const allStreets = [
+  new Street("Ocean Avenue", 1999, 1.1, 4),
+  new Street("Evergreen", 2008, 2.7, 2),
+  new Street("4th Street", 2015, 0.8),
+  new Street("Sunset Boulevard", 1982, 2.5, 5)
+];
+
+function reportParks(p) {
+  console.log("--------PARKS REPORT----");
+  //Density
+  p.forEach(el => el.treeDensity());
+  //Average age
+
+  //Which park has more than 1000 trees
+}
+
+function reoprtStreets(s) {}
+
+reportParks(allParks);
+reoprtStreets(allStreets);
